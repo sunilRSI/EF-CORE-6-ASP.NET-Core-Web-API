aee1ba5708f700d172bf9a9ed7588322b775e37f
Sunil Ghutukade <Sunil.Ghutukade@grassvalley.com>
2023-12-07 14:34:25 +0530

    main initial commit

---
+++ BikeStores.MSSQL/BikeStores.MSSQL/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://127.0.0.1.com");
+                      });
+});
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BikeStores.MSSQL.Data.BikestoresContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("BikeStoresDB"));
+    });
+
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapControllers();
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,18 @@
+# BikeStores WEB API
+
+![Alt Text](https://github.com/alkaren/EF-CORE-6-ASP.NET-Core-Web-API/blob/main/example.png)
+
+## Using
+- SQL Server
+- EF Core 6
+- ASP.NET Core Web API
+- CRUD
+- SWAGGER
+
+## Packages
+The package reference is available
+<ul>
+  <li><a href="https://www.nuget.org/packages/Microsoft.EntityFrameworkCore.Design/">Microsoft.EntityFrameworkCore.Design</a></li>
+  <li><a href="https://www.nuget.org/packages/Microsoft.EntityFrameworkCore.SqlServer/">Microsoft.EntityFrameworkCore.SqlServer</a></li>
+  <li><a href="https://www.nuget.org/packages/Microsoft.AspNetCore.Mvc.NewtonsoftJson/">Microsoft.AspNetCore.Mvc.NewtonsoftJson</a></li>
+</ul>
